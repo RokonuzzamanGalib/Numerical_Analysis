1104833d8376f129ce325c755c6265ba47cf675f
RokonuzzamanGalib <rokonuzzamangalib@gmail.com>
2025-01-29 02:14:29 +0600

    Find Roots By Bisection Method
    (1)   x*x*x-2*x-5  =0
    (2)   x*x*x + x*x + x + 7=0

---
+++ Bisection/Bisection_Method_2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+#define EP 0.0001
+
+double solution(double x) {
+   return x*x*x + x*x + x + 7;
+}
+
+
+void Bisection(double a, double b) {
+
+   if (solution(a) * solution(b) >= 0) {
+      cout << "There is no root between a and b\n";
+      return;
+   }
+
+   double m = a;
+
+   while ((b-a) >= EP) {
+    
+      m = (a+b)/2;
+     
+      if (solution(m) == 0.0)
+         break;
+    
+      else if (solution(m)*solution(a) < 0)
+         b = m;
+
+      else
+         a = m;
+   }
+   cout << "The value of root is : " << m;
+}
+
+int main() {
+   double a =-3;
+   double b =-2;
+   Bisection(a, b);
+   return 0;
+}
